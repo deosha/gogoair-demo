16178a019708ea7a1171afccaf260bf2af44f99d
Deo Shankar <jha.deo1771@gmail.com>
2019-06-24 18:20:06 +0530

    fixed list error

--- infrastructure_automation/modules/Instances-And-LoadBalanacers/main.tf
+++ infrastructure_automation/modules/Instances-And-LoadBalanacers/main.tf
@@ -127,16 +127,4 @@ resource "aws_alb_listener" "alb-listener" {
   }
 }
 
-resource "aws_lb_listener" "alb-listener-https" {
-  load_balancer_arn = "${aws_alb.alb.arn}"
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = "${aws_alb_target_group.alb-tg.arn}"
-  }
-}
-
-
 
